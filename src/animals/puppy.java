ebf299e9325b0ecba666d1d4ce0134821e4a862f
DELTALABORATORIES <deathadderdelta@gmail.com>
2017-12-08 23:03:41 +0400

    Homework 7

---
+++ src/animals/puppy.java
@@ -0,0 +1,18 @@
+package animals;
+
+public class puppy {
+
+    private String name;
+
+    public puppy(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
